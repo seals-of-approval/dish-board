13ffceacccce5344dfad8f94a36be1372bd2a1c0
msachi <marchee88@gmail.com>
2016-12-07 15:23:01 +0000

    Set up JWT options and encoding

--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 const Hapi = require('hapi');
+const routes = require('./routes.js');
 const Inert = require('inert');
 const CookieAuth = require('hapi-auth-cookie');
+const jwt = require('json-web-token');
+const env = require('env2');
+
+env('./config.env');
+
 const server = new Hapi.Server();
 
-const routes = require('./routes.js');
+// const token = '191f179355b4e4f1514a66d747986e932985c735';
+const secret = process.env.CLIENT_SECRET;
+
+const payload = {
+  'typ': 'JWT',
+  'sub': 'user-info',
+  'exp': Date.now() + 24 * 60 * 60 * 1000,
+  'iat': Date.now()
+};
+
+const algorithm = 'HS256';
+
+jwt.encode(secret, payload, algorithm, function (err, token) {
+  if (err) {
+    return console.error(err.name, err.message);
+  } else {
+    console.log(token);
+  }
+});
 
 const defaultRoute = {
   method: 'GET',
