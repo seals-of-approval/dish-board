7907814d96465687026279eaea939551ab3339db
jsms90 <jsms@hotmail.co.uk>
2016-12-07 16:04:01 +0000

    add welcome route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const login = require('./routes/login');
+const welcome = require('./routes/welcome');
 
 module.exports = [
-  login
+  login,
+  welcome
 ];
---
+++ src/routes/welcome.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const env = require('env2')('./config.env');
+
+const setOptions = (code) => {
+  const options = {
+    url: 'https://github.com/login/oauth/access_token',
+    method: 'POST',
+    headers: {
+      accept: 'application/json'
+    },
+    form: {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: `${process.env.BASE_URL}/welcome`,
+      code: code
+    }
+  };
+  return options;
+};
+
+const welcome = {
+  method: 'GET',
+  path: '/welcome',
+  handler: (req, reply) => {
+    const myOptions = setOptions(req.query.code);
+    request(myOptions, (err, res, body) => {
+      if (err) throw err;
+      const myToken = JSON.parse(body).access_token;
+      console.log(`By the way, we've got the token: ${myToken}`);
+      reply('worked!');
+    });
+    // reply.view('profile')
+  }
+};
+
+module.exports = welcome;
