d241612e3ebd4a35df19fb5c65d509a89dafb6c2
Nick Field <heyimnick@live.com>
2016-12-07 14:24:03 +0000

    add Hapi,Inert,CookieAuth dependencies. Set up server to handle routes, and default route to handle static files

--- src/server.js
+++ src/server.js
@@ -1,1 +1,26 @@
-const http = require('http');
+const Hapi = require('hapi');
+const Inert = require('inert');
+const CookieAuth = require('hapi-auth-cookie');
+const server = new Hapi.Server();
+
+const routes = require('./routes.js');
+
+const defaultRoute = {
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: './public'
+    }
+  }
+};
+
+server.connection({ port: process.env.PORT || 4000 });
+
+server.register([Inert, CookieAuth], (registerError) => {
+  if (registerError) throw registerError;
+
+  server.route([defaultRoute, ...routes]);
+});
+
+module.exports = server;
