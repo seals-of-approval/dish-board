7f63ba903807a2fb1e1a46b7bbc9deb7e761577c
msachi <marchee88@gmail.com>
2016-12-09 15:19:57 +0000

    add SGC photo

--- src/helpers/json-to-profile-object.js
+++ src/helpers/json-to-profile-object.js
@@ -1,26 +1,23 @@
-const mapForProfile = (issues) => {
-  issues = issues.map((issue) => {
-    if (issue.labels[0]) {
-      issue.label = issue.labels[0].name;
-    }
-    issue.repo = issue.repository.name;
-    if (issue.assignee) {
-      issue.assignee = issue.assignee.login;
-    }
-    return issue;
-  });
-  return issues;
-};
 module.exports = (user, closedissues, openissues) => {
+  const mapForProfile = (issues) => {
+    issues = issues.map((issue) => {
+      if (issue.labels[0]) {
+        issue.label = issue.labels[0].name;
+      }
+      issue.repo = issue.repository.name;
+      if (issue.assignee) {
+        issue.assignee = issue.assignee.login;
+      }
+      return issue;
+    }).filter(i => !i.pull_request);
+    return issues;
+  };
   user = JSON.parse(user);
-  openissues = mapForProfile(JSON.parse(openissues));
-  closedissues = mapForProfile(JSON.parse(closedissues));
-  const issues = openissues.concat(closedissues);
   const profileObj = {};
   profileObj.username = user.login;
   profileObj['user-url'] = user.html_url;
+  profileObj['open-issues'] = mapForProfile(JSON.parse(openissues)).filter(i => i.state === 'open');
+  profileObj['closed-issues'] = mapForProfile(JSON.parse(closedissues)).filter(i => i.state === 'closed');
   profileObj.organisation = issues[0].repository.owner.login;
-  profileObj['open-issues'] = issues.filter(i => i.state === 'open' && !i.pull_request);
-  profileObj['closed-issues'] = issues.filter(i => i.state === 'closed' && !i.pull_request);
   return profileObj;
 };
