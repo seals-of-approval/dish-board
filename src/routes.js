40753b250334626eb5895b720343f25ac3f3c689
Nick Field <heyimnick@live.com>
2016-12-09 11:46:02 +0000

    add logout route, added to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const login = require('./routes/login');
 const welcome = require('./routes/welcome');
 const profile = require('./routes/profile');
+const logout = require('./routes/logout');
 
 module.exports = [
   login,
   welcome,
-  profile
+  profile,
+  logout
 ];
